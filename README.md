cde2407663af818733f43b7114c0e82d8a87bfb1
Toughee <Jimernez.io@gmail.com>
2021-08-01 10:32:59 -0500

    CarTraced/004/Minor_Update_001 - Improved UI for app - Updated doc files for future bug reports

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-![App logo](./public/img/CarTraced.png)
-
-
 # CarTraced
 
-Complete Car classifier created using React and TensorFlow
+Car classifier app created using React for the Front end interface and TensorFlow for back end machine learning models. 
+
+![Splash logo](./public/img/splashpageCT.png)
+
+## Future Updates / Roadmap for app
+
+- Image preview displaying inside app
+- Picture gets analyzed successfully with TF and 
+- TF only analyses car pictures. Returns an error message for other pictures
+- Load TF models independently
+- Add drag and drop for picture upload
--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import React from 'react';
-import { useState } from "react";
+// import red from './red.jpeg';
+// import { useState } from "react";
 import * as tf from '@tensorflow/tfjs'
 import * as mobilenet from '@tensorflow-models/mobilenet'
 
-// function imgUpload() {
-//   alert('random name set');
-// }
-
-class App extends React.Component {
+class imgTraceModel extends React.Component {
   state = {
     isTfReady: false,
     isModelReady: false,
@@ -26,41 +23,28 @@ class App extends React.Component {
     this.setState({ isModelReady: true })
   }
 
-
-// const img = document.getElementById('img');
-
-// const model = async() => {
-//   await mobilenet.load();
-// }
-
-// const predictions = async() => {
+// const classifierCateg = async() => {
 //  await model.classify(img);
 // }
-//const predictions = "test";
-// console.log(predictions);
-// console.log('Predictions: ');
-
+//const classifierCateg = "test";
+// console.log(classifierCateg);
+// console.log('classifierCateg: ');
 
 // const [imgloc, setimgloc] = useState(null);
-
 // const imgUpload = event => { 
 //   const { files } = event.target;
-
-  
 // }
-
 render() {
-  // const {classification, isTfReady, isModelReady, image} = this.state;
   return (
     <div className="container">
       <header>
-              <h1>CarTraced</h1>
+            <h1>CarTraced</h1>
             <h2>Classify car models and details using tensorflow</h2>
       </header>
       <br/>
- <article className="imageSection leftbox">
-
-
+ <article className="dataSection ">
+ <aside>
+ <button>Models loaded </button>
         <button>
             <input
             type="file"
@@ -69,27 +53,30 @@ render() {
             // onChange={imgUpload}
           />
         </button>
+        <button>Make:  </button>
+        <button>Model: </button>
+        <button>Car Type: </button>
+        <button>Color: </button>
+        
+</aside>
+        {/* <img
+              src={picupload}
+            /> */}
+            <aside>
+                    {/* <img
+              src={red}
+            /> */}
+            </aside>
+        {/* <p>{Classifier category: Make:}</p> */}
+        {/* <p>{Classifier category: Model:}</p> */}
+        {/* <p>{Classifier category: CarType:}</p> */}
+        {/* <p>{Classifier category: Color}</p> */}
 
-        <p>{this.state.isTfReady ? <span>TFJS ready</span> : ''} & {this.state.isModelReady ? <span> TSmodels Ready</span> : <span>loading...</span>}</p>
-
-        <p>Make:</p>
-        <p>Model:</p>
-        <p>Car Type:</p>
-        <p>Color:</p>
-        {/* <p>{predictions}</p> */}
-
-      </article>
-
-        <article className="imageSection rightbox">
-          <img
-              // src={picSrc}
-            />
         </article>
 
-
-    </div>
-  )
+      </div>
+      )
+    }
 }
-}
 
-export default App
+export default imgTraceModel
